170b6e6a5e33bca1646efdb45b667487980c2a57
bwanaballen <bwanaballen@ba32571f-2d31-0410-81bd-572470cdafca>
2007-06-02 16:35:50 +0000

    git-svn-id: http://erlmagic.googlecode.com/svn/trunk@11 ba32571f-2d31-0410-81bd-572470cdafca

--- util.erl
+++ util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([join/2, test_join/0]).
+-export([join/2, capitalize/1, test_join/0]).
 
 join(C, L) ->
     join(C, L, []).
@@ -10,6 +10,14 @@ join(_, [], Acc) ->
 join(C, [H|L], Acc) ->
     Acc1 = [H, C|Acc],
     join(C, L, Acc1).
+
+capitalize(Word) ->
+    FirstLetter = hd(Word),
+    if FirstLetter > 96 ->
+	    [hd(Word) - 32] ++ tl(Word);
+       true ->
+	    Word
+    end.
 
 test_join() ->
     L = ["this", "that", "the", "other"],
