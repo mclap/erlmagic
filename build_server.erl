28fda9326733d9725dee78813aa86ef35d5e4ebf
bwanaballen <bwanaballen@ba32571f-2d31-0410-81bd-572470cdafca>
2007-06-02 04:00:52 +0000

    git-svn-id: http://erlmagic.googlecode.com/svn/trunk@5 ba32571f-2d31-0410-81bd-572470cdafca

--- build_server.erl
+++ build_server.erl
@@ -1,5 +1,5 @@
 -module(build_server).
--export([test/0]).
+-export([make_all/4]).
 
 get_type("void") ->
     "";
@@ -62,7 +62,7 @@ param_base_type(T) ->
     case string:substr(Rval, 1, 2) of
 	"??" ->
 	    %io:format("Rval = ~s~n", [Rval]),
-	    throw("Bad param");
+	    throw({"Bad param", Rval});
 	_ ->
 	    Rval
     end.
@@ -73,17 +73,17 @@ param_name(H) ->
 	nil ->
 	    nil;
         [$*|_] ->
-	    throw("Bad param");
+	    throw({"Bad param", R});
 	_ ->
 	    string:strip(string:strip(R, both, $&), both, $_)
     end.
 
-make_param(L, N) ->
-    make_param(L, N, [], []).
+make_param(L, N, Param_fun) ->
+    make_param(L, N, Param_fun, [], []).
 
-make_param([], _, Acc1, Acc2) ->
+make_param([], _, _, Acc1, Acc2) ->
     {lists:reverse(Acc1), lists:reverse(Acc2)};
-make_param([H|L], N, Acc1, Acc2) ->
+make_param([H|L], N, Param_fun, Acc1, Acc2) ->
     Pname = param_name(H),
     T = get_type(chp2:param_type(H)),
     case param_base_type(chp2:param_type(H)) of 
@@ -91,46 +91,34 @@ make_param([H|L], N, Acc1, Acc2) ->
 	    Acc21 = Acc2,
 	    Acc11 = Acc1;
 	Pname_type ->
-	    Acc21 = [lists:flatten(io_lib:format("\t\t~s ~s = ~s(erl_element(~B, msg));~n", [T, Pname, Pname_type, N]))|Acc2],
+	    Acc21 = [lists:flatten(Param_fun(T, Pname, Pname_type, N))|Acc2],
 	    Acc11 = [param_name(H) | Acc1]
     end,
-    make_param(L, N + 1, Acc11, Acc21).
+    make_param(L, N + 1, Param_fun, Acc11, Acc21).
 
 bad_cmd(Command) ->
     lists:member(Command, ["compose", "read"]).
 
-print_param(L) ->
-    io:format("~s", [L]).
 
-make_one(Row) ->
+make_one(Row, Body_fun, Param_fun, Bad_param_fun) ->
     %io:format("~p~n", [Row]),
     Command = chp2:name(Row),
     try 
 	case bad_cmd(Command) of 
 	    false ->
-		{Param_names, Param_lines} = make_param(chp2:param_vals(Row), 3), 
-		Parameters = util:join(",", Param_names),
-		io:format("\tif (command == \"" ++ Command ++ "\") {~n",[]),
-		io:format("\t\tint iid = ERL_INT_VALUE(erl_element(2, msg));~n", []),
-		io:format("\t\tImage image = image_list[iid];~n"), 
-		lists:foreach(fun print_param/1, Param_lines),
-		io:format("\t\timage.~s(~s);~n", [Command, Parameters]),
-		io:format("\t\timage_list[iid] = image;~n", []),
-		io:format("\t\terl_send(fd, pid, ok);~n",[]),
-		io:format("\t}~n",[]);
+		{Param_names, Param_lines} = make_param(chp2:param_vals(Row), 3, Param_fun), 
+		Body_fun(Command, Param_names, Param_lines);
 	    true ->
 		io:format("\t\t//~s not implemented~n", [Command])
 	end
     catch
-	throw:"Bad param" ->
-	    io:format("\t\t//~s not implemented because of parameters~n", [Command])
+	throw:{"Bad param", Reason} ->
+	    Bad_param_fun(Command, Reason)
     end.
 
-make_all(F) ->
-    lists:foreach(fun make_one/1, chp2:parse_file(F)).
-
-test() ->
-    make_all("image.h").
-
+make_all(F, Body_fun, Param_fun, Bad_param_fun) ->
+    MakeFunc =  fun(Bf, Pf, Bpf) -> (fun(Row) -> make_one(Row, Bf, Pf, Bpf) end) end,
+    Func = MakeFunc(Body_fun, Param_fun, Bad_param_fun),
+    lists:foreach(Func, chp2:parse_file(F)).
 
 	      
