eed9582d563a46562263fdb5a0483206ad7ceae8
bwanaballen <bwanaballen@ba32571f-2d31-0410-81bd-572470cdafca>
2007-06-08 21:29:19 +0000

    make util join and capitalize simpler
    git-svn-id: http://erlmagic.googlecode.com/svn/trunk@21 ba32571f-2d31-0410-81bd-572470cdafca

--- util.erl
+++ util.erl
@@ -1,22 +1,16 @@
 -module(util).
 -export([join/2, capitalize/1, make_command_name/2, test_join/0]).
 
-join(C, L) ->
-    join(C, L, []).
-join(_, [], []) ->
-    "";
-join(_, [], Acc) ->
-    lists:flatten(tl(lists:reverse(Acc)));
-join(C, [H|L], Acc) ->
-    Acc1 = [H, C|Acc],
-    join(C, L, Acc1).
+join([F|L], Sep) ->
+    lists:foldl(fun(X, Acc) -> Acc ++ Sep ++ X end, F, L).
 
-capitalize(Word) ->
-    FirstLetter = hd(Word),
-    if FirstLetter > 96 ->
-	    [hd(Word) - 32] ++ tl(Word);
+capitalize([]) ->
+    [];
+capitalize([F|L]) ->
+    if F > 96 ->
+	    [F - 32] ++ L;
        true ->
-	    Word
+	    [F] ++ L
     end.
 
 make_command_name(Command, Params) ->
