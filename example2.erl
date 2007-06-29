d876670ea11730fb4669b952e7c2cb59ceac4c37
bwanaballen <bwanaballen@ba32571f-2d31-0410-81bd-572470cdafca>
2007-06-29 20:56:43 +0000

    add example2
    git-svn-id: http://erlmagic.googlecode.com/svn/trunk@55 ba32571f-2d31-0410-81bd-572470cdafca

--- example2.erl
+++ example2.erl
@@ -1,14 +1,18 @@
 -module(example2).
 -compile(export_all).
 
-do_one(Source_dir, Dest_dir, Name) ->
+do_one(Source_dir, Dest_dir, Host, Name) ->
+    Image = imagelib:read(filename:join(Source_dir, Name)),
+    imagelib:scale(Host, Image, "30%"),
+    imagelib:border(Host, Image, "2x2"),
+    imagelib:sharpen(Host, Image, 0.0, 1.0),
     io:format("copy " ++ Source_dir ++ "/" ++ Name ++ " to " ++ Dest_dir ++ "/" ++ Name ++ "~n", []).
 
 start() ->
     Source_dir = ".",
     Dest_dir = ".",
-    MakeDoOne = fun(Source, Dest) -> (fun(N) -> do_one(Source, Dest, N) end) end,
-    DoOne = MakeDoOne(Source_dir, Dest_dir),
+    MakeDoOne = fun(Source, Dest, Host) -> (fun(N) -> do_one(Source, Dest, Host, N) end) end,
+    DoOne = MakeDoOne(Source_dir, Dest_dir, imagelib:start()),
     lists:foreach(DoOne, [filename:basename(X) || X <- filelib:wildcard(Source_dir ++ "/*.jpg")]).
  
 			
